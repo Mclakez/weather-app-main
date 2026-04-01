3e2ccc33de1874c5479fc2ca3a2b7fd0d987d526
Mclakez <129914833+Mclakez@users.noreply.github.com>
2026-04-01 22:42:03 +0100

    Change weather icon paths to relative URLs
    Updated image paths to be relative.

--- src/HourlyForecast.jsx
+++ src/HourlyForecast.jsx
@@ -3,21 +3,21 @@ import {useState} from "react"
 import "./index.css"
 
 const weatherIcons = {
-    0: "/assets/images/icon-sunny.webp",
-    1: "/assets/images/icon-partly-cloudy.webp",
-    2: "/assets/images/icon-cloudy.webp",
-    3: "/assets/images/icon-overcast.webp",
-    45: "/assets/images/icon-fog.webp",
-    48: "/assets/images/icon-fog.webp",
-    51: "/assets/images/icon-drizzle.webp",
-    53: "/assets/images/icon-drizzle.webp",
-    55: "/assets/images/icon-drizzle.webp",
-    61: "/assets/images/icon-rain.webp",
-    63: "/assets/images/icon-rain.webp",
-    65: "/assets/images/icon-rain.webp",
-    71: "/assets/images/icon-snow.webp",
-    73: "/assets/images/icon-snow.webp",
-    75: "/assets/images/icon-snow.webp",
+    0: "./assets/images/icon-sunny.webp",
+    1: "./assets/images/icon-partly-cloudy.webp",
+    2: "./assets/images/icon-cloudy.webp",
+    3: "./assets/images/icon-overcast.webp",
+    45: "./assets/images/icon-fog.webp",
+    48: "./assets/images/icon-fog.webp",
+    51: "./assets/images/icon-drizzle.webp",
+    53: "./assets/images/icon-drizzle.webp",
+    55: "./assets/images/icon-drizzle.webp",
+    61: "./assets/images/icon-rain.webp",
+    63: "./assets/images/icon-rain.webp",
+    65: "./assets/images/icon-rain.webp",
+    71: "./assets/images/icon-snow.webp",
+    73: "./assets/images/icon-snow.webp",
+    75: "./assets/images/icon-snow.webp",
 };
 
 // function getWeatherIcon(code) {
