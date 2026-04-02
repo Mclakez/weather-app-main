3c4d030c9eb06c99bf22e4de7d80f3985f70c70f
Mclakez <129914833+Mclakez@users.noreply.github.com>
2026-04-02 21:07:44 +0100

    Update CurrentWeather component with new weather logic and styles

--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -1,6 +1,12 @@
 import bigBg from "/assets/images/bg-today-large.svg";
+import iconSunny from "/assets/images/icon-sunny.webp";
+import iconPartlyCloudy from "/assets/images/icon-partly-cloudy.webp";
+import iconOvercast from "/assets/images/icon-overcast.webp";
+import iconFog from "/assets/images/icon-fog.webp";
+import iconDrizzle from "/assets/images/icon-drizzle.webp";
+import iconRain from "/assets/images/icon-rain.webp";
+import iconSnow from "/assets/images/icon-snow.webp";
 import './App.css'
-// import smallBg from "/assets/images/bg-today-small.svg";
 
 export default function CurrentWeather({ weather, error , loading, values}) {
     if(loading) {
@@ -22,48 +28,46 @@ export default function CurrentWeather({ weather, error , loading, values}) {
 
     const date = new Date(weather.current.time)
 
- const formattedDate = date.toLocaleDateString("en-US", {
-   weekday: "long",
-   month: "long",
-   day: "numeric",
-   year: "numeric"
- })
+    const formattedDate = date.toLocaleDateString("en-US", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric"
+    })
 
- let weatherIcon;
- if (weather.current.weather_code === 0) {
-    weatherIcon = "/assets/images/icon-sunny.webp"
- } else if (weather.current.weather_code === 1) {
-    weatherIcon = "/assets/images/icon-partly-cloudy.webp"
- } else if (weather.current.weather_code === 2) {
-    weatherIcon = "/assets/images/icon-overcast.webp"
- } else if (weather.current.weather_code === 3) {
-    weatherIcon = "/assets/images/icon-overcast.webp"
- } else if (weather.current.weather_code === 45 || weather.current.weather_code === 48) {
-    weatherIcon = "/assets/images/icon-fog.webp"
- } else if (weather.current.weather_code === 51 || weather.current.weather_code === 53 || weather.current.weather_code === 55) {
-    weatherIcon = "/assets/images/icon-drizzle.webp"
- } else if (weather.current.weather_code === 61 || weather.current.weather_code === 63 || weather.current.weather_code === 65) {
-    weatherIcon = "/assets/images/icon-rain.webp"
- } else if (weather.current.weather_code === 71 || weather.current.weather_code === 73 || weather.current.weather_code === 75) {
-    weatherIcon = "/assets/images/icon-snow.webp"
- } else {
-    weatherIcon = "/assets/images/icon-sunny.webp"
- }
- //not the solution i wanted, it should get it using getCity
+    let weatherIcon;
+    if (weather.current.weather_code === 0) {
+        weatherIcon = iconSunny;
+    } else if (weather.current.weather_code === 1) {
+        weatherIcon = iconPartlyCloudy;
+    } else if (weather.current.weather_code === 2) {
+        weatherIcon = iconOvercast;
+    } else if (weather.current.weather_code === 3) {
+        weatherIcon = iconOvercast;
+    } else if (weather.current.weather_code === 45 || weather.current.weather_code === 48) {
+        weatherIcon = iconFog;
+    } else if (weather.current.weather_code === 51 || weather.current.weather_code === 53 || weather.current.weather_code === 55) {
+        weatherIcon = iconDrizzle;
+    } else if (weather.current.weather_code === 61 || weather.current.weather_code === 63 || weather.current.weather_code === 65) {
+        weatherIcon = iconRain;
+    } else if (weather.current.weather_code === 71 || weather.current.weather_code === 73 || weather.current.weather_code === 75) {
+        weatherIcon = iconSnow;
+    } else {
+        weatherIcon = iconSunny;
+    }
 
     return(
         <div>
             <div style={{ backgroundImage: `url(${bigBg})` }} className=" bg-cover bg-center flex flex-col md:flex-row items-center justify-center md:justify-between h-52 pt-4 md:pt-0 px-4 rounded-lg gap-4 md:gap-8 font-sans">
-            <div className="flex  flex-col items-start gap-2">
-               
-                <p className="font-500 text-xl ">{values?.cityName ?? "Berlin"}, {values?.country ?? "Germany"}</p>
-                <p className="text-sm font-sans">{formattedDate}</p>
+                <div className="flex  flex-col items-start gap-2">
+                    <p className="font-500 text-xl ">{values?.cityName ?? "Berlin"}, {values?.country ?? "Germany"}</p>
+                    <p className="text-sm font-sans">{formattedDate}</p>
+                </div>
+                <div className="flex items-center">
+                    <img src={weatherIcon} className="w-24"/>
+                    <h2 className="text-7xl italic">{weather.current.temperature_2m}&deg;</h2>
+                </div>
             </div>
-            <div className="flex items-center">
-                <img src={weatherIcon} className="w-24"/>
-                <h2 className="text-7xl italic">{weather.current.temperature_2m}&deg;</h2>
-            </div>
-        </div>
         </div>
     )
 }
